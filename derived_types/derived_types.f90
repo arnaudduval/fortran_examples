936910b10aaf4165c580a1fa49ba48497c005638
Arnaud Duval <arnaud.duval@gmail.com>
2023-12-12 21:54:05 +0100

    Derived types : add an example with allocatable arrays.

--- derived_types/derived_types.f90
+++ derived_types/derived_types.f90
@@ -1,60 +1,85 @@
 module types
-    type color
-        character(len=25) :: name
-        integer, dimension(3) :: code_rvb
-    end type color
+    type nurbspatch
+        integer :: dimension
+        integer, dimension(:), allocatable :: Jpqr         !! degrees
+        double precision, dimension(:), allocatable :: Ukv1, Ukv2, Ukv3
 
-    type pen
-        character(len=25) :: name
-        integer :: thickness
-    end type pen
+        contains
+
+        procedure :: initPatch
+
+    end type nurbspatch
+
+    contains
+        subroutine initPatch(self, dim, Jpqr, Ukv, kv_sizes)
+            implicit none
+
+            class(nurbspatch) :: self
+            integer :: dim
+            integer, dimension(dim) :: Jpqr
+            integer, dimension(dim) :: kv_sizes
+            double precision, dimension(:) :: Ukv
+
+            self%dimension = dim
+
+            if (allocated(self%Jpqr)) deallocate(self%Jpqr)
+            allocate(self%Jpqr(dim))
+            self%Jpqr(:) = Jpqr(:)
+
+            if (allocated(self%Ukv1)) deallocate(self%Ukv1)
+            allocate(self%Ukv1(kv_sizes(1)))
+            self%Ukv1(:) = Ukv(:kv_sizes(1))
+
+            if (dim > 1) then
+                if (allocated(self%Ukv2)) deallocate(self%Ukv2)
+                allocate(self%Ukv2(kv_sizes(2)))
+                self%Ukv2(:) = Ukv(kv_sizes(1)+1:kv_sizes(1)+kv_sizes(2))
+            endif
+
+            if (dim > 2) then
+                if (allocated(self%Ukv3)) deallocate(self%Ukv3)
+                allocate(self%Ukv3(kv_sizes(3)))
+                self%Ukv3(:) = Ukv(kv_sizes(1)+kv_sizes(2)+1:   &
+                                   kv_sizes(1)+kv_sizes(2)+kv_sizes(3))
+            endif
+
+        end subroutine initPatch
 end module types
 
 
 
 program test
-    use types, only: color, pen
+    use types, only: nurbspatch
 
     implicit none
 
-    type(color), dimension(3) :: tabrvb
-    type(pen) :: crayon
+    type(nurbspatch) :: patch
 
-    tabrvb =    &
-        (/ color("Red", (/ 255,0,0 /)),                 &
-           color("Green", (/ 0,255,0 /)),               &
-           color("Blue", (/ 0,0,255 /)) /)
+    call patch%initPatch(3, (/ 2, 2, 2 /),       &
+                    &   (/ 0.0D0, 0.0D0, 0.0D0, 0.25D0, 0.5D0, 0.75D0, 1.0D0, 1.0D0, 1.0D0, 0.0D0, 0.0D0, 0.0D0, &
+                    &      0.5D0, 1.0D0, 1.0D0, 1.0D0, 0.0D0, 0.0D0, 0.0D0, 1.0D0, 1.0D0, 1.0D0 /), &
+                    &   (/ 9, 7, 6 /) )
 
-    crayon = pen("HB5", 12)
+    write(*,'(10F5.2)') patch%Ukv1
+    write(*,'(10F5.2)') patch%Ukv2
+    write(*,'(10F5.2)') patch%Ukv3
 
-
-    write(*,*) tabrvb(1)
-    write(*,*) crayon
-
-    call printcolor(tabrvb(1))
-    call print_color_and_pen(tabrvb(1), crayon)
+    call manip(patch)
 
 end program test
 
 
-subroutine printcolor(col)
-    use types, only: color
+subroutine manip(patch)
+    use types, only: nurbspatch
 
     implicit none
 
-    type(color) :: col
+    type(nurbspatch) :: patch
 
-    write(*,*) col%name, col%code_rvb
-end subroutine printcolor
+    write(*,*) 'In subroutine'
+    write(*,'(10F5.2)') patch%Ukv1
+    write(*,'(10F5.2)') patch%Ukv2
+    write(*,'(10F5.2)') patch%Ukv3
 
-subroutine print_color_and_pen(col, stylo)
-    use types, only: color, pen
 
-    implicit none
-
-    type(color) :: col
-    type(pen) :: stylo
-
-    write(*,*) col%name, col%code_rvb
-    write(*,*) stylo%name, stylo%thickness
-end subroutine print_color_and_pen
+end subroutine manip
